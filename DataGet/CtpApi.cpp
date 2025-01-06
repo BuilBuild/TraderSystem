d2bc22963addafb05d5b63369fa4eb033bbc3a3a
BuiBuild <lei15557570906@outlook.com>
2025-01-06 20:02:55 +0800

    APIBase

---
+++ DataGet/CtpApi.cpp
@@ -0,0 +1,25 @@
+/*
+ * @Author: LeiJiulong
+ * @Date: 2025-01-06 19:46:40
+ * @LastEditors: LeiJiulong && lei15557570906@outlook.com
+ * @LastEditTime: 2025-01-06 19:46:41
+ * @Description: 
+ */
+#include "CtpApi.h"
+
+CtpApi::CtpApi()
+    : APIBase()
+{
+}
+
+CtpApi::~CtpApi()
+{
+}
+
+void CtpApi::onOrderBookReceive()
+{
+}
+
+void CtpApi::init()
+{
+}
